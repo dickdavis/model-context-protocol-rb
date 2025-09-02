fb9b634ce54d1cfed8c2f45ebe1ecefb92efe2e8
Dick Davis <dick@hey.com>
2025-09-01 21:27:57 -0500

    Add mock_redis for testing

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,6 +3,8 @@
 Dir[File.expand_path("../lib/**/*.rb", __dir__)].sort.each { |f| require f }
 Dir[File.expand_path("spec/support/**/*.rb")].sort.each { |file| require file }
 
+require "mock_redis"
+
 RSpec.configure do |config|
   # Enable flags like --only-failures and --next-failure
   config.example_status_persistence_file_path = ".rspec_status"
