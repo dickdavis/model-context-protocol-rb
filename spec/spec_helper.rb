cb2fc86782271304a312fd99c26a088ab118ce89
Dick Davis <dick@hey.com>
2025-09-15 18:53:07 -0500

    Stub redis with mock redis in specs

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,18 +1,27 @@
-# frozen_string_literal: true
+require "mock_redis"
+
+Object.send(:remove_const, :Redis) if Object.const_defined?(:Redis)
+Object.const_set(:Redis, MockRedis)
 
 Dir[File.expand_path("../lib/**/*.rb", __dir__)].sort.each { |f| require f }
 Dir[File.expand_path("spec/support/**/*.rb")].sort.each { |file| require file }
 
-require "mock_redis"
-
 RSpec.configure do |config|
-  # Enable flags like --only-failures and --next-failure
   config.example_status_persistence_file_path = ".rspec_status"
 
-  # Disable RSpec exposing methods globally on `Module` and `main`
   config.disable_monkey_patching!
 
   config.expect_with :rspec do |c|
     c.syntax = :expect
+  end
+
+  config.before(:each) do
+    if defined?(Redis.current)
+      begin
+        Redis.current.flushdb
+      rescue
+        nil
+      end
+    end
   end
 end
