780a26e159422af95de5c12318d83cc32a3b00f7
Dick Davis <dick@hey.com>
2025-09-27 03:59:06 -0500

    Update server logger to include jsonrpc request ID in log entries

--- lib/model_context_protocol/server/server_logger.rb
+++ lib/model_context_protocol/server/server_logger.rb
@@ -17,7 +17,11 @@ module ModelContextProtocol
       self.formatter = formatter || proc do |severity, datetime, progname, msg|
         timestamp = datetime.strftime("%Y-%m-%d %H:%M:%S.%3N")
         prog_name = progname ? "[#{progname}]" : ""
-        "[#{timestamp}] #{prog_name} #{severity}: #{msg}\n"
+        mcp_context = Thread.current[:mcp_context]
+        request_id = mcp_context&.dig(:jsonrpc_request_id)
+        request_id_str = request_id ? " [#{request_id}]" : ""
+
+        "[#{timestamp}] #{prog_name}#{request_id_str} #{severity}: #{msg}\n"
       end
     end
   end
