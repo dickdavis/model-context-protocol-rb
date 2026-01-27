f45e046fd6ba48aa5cebca0da32dc0c7546224ed
Dick Davis <dick@hey.com>
2026-01-26 23:33:25 -0600

    Implement scaffolding for RSpec matchers to test MCP handlers

--- lib/model_context_protocol.rb
+++ lib/model_context_protocol.rb
@@ -1,6 +1,9 @@
 require "addressable/template"
 
-Dir[File.join(__dir__, "model_context_protocol/", "**", "*.rb")].sort.each { |file| require_relative file }
+Dir[File.join(__dir__, "model_context_protocol/", "**", "*.rb")]
+  .reject { |file| file.include?("/rspec") }
+  .sort
+  .each { |file| require_relative file }
 
 ##
 # Top-level namespace
---
+++ lib/model_context_protocol/rspec/matchers.rb
@@ -0,0 +1,9 @@
+require_relative "matchers/be_valid_mcp_class"
+
+module ModelContextProtocol
+  module RSpec
+    module Matchers
+      # Include all matchers when this module is included
+    end
+  end
+end
